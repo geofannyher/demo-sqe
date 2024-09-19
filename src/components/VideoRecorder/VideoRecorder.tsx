d2b6d07c645db2152f2df14ee31e772b2d7ec352
geofannyher <geofannyher@gmail.co>
2024-09-19 14:32:44 +0700

    update

--- src/components/VideoRecorder/VideoRecorder.tsx
+++ src/components/VideoRecorder/VideoRecorder.tsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+
 const VideoRecorder = ({ videoSrc }: any) => {
+  const [currentSrc, setCurrentSrc] = useState(videoSrc);
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    setFade(true); // Mulai fade-out ketika video source berubah
+    const timeoutId = setTimeout(() => {
+      setCurrentSrc(videoSrc); // Ganti video setelah fade-out
+      setFade(false); // Mulai fade-in setelah source diganti
+    }, 300); // Durasi fade-out
+
+    return () => clearTimeout(timeoutId);
+  }, [videoSrc]);
+
   return (
-    <>
+    <div
+      className={`video-container transition-opacity duration-500 ease-in-out ${
+        fade ? "opacity-0" : "opacity-100"
+      }`}
+    >
       <video
-        src={videoSrc}
+        src={currentSrc}
         controls={false}
         className="rounded-lg max-w-xl mx-auto height-large width-large"
         style={{
@@ -15,7 +34,7 @@ const VideoRecorder = ({ videoSrc }: any) => {
         loop={true}
         muted={true}
       />
-    </>
+    </div>
   );
 };
 
