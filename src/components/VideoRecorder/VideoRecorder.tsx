ecfefb3d3d2c92f5803227a32930fadbdc093afd
geofannyher <geofannyher@gmail.co>
2024-09-26 11:17:58 +0700

    update

--- src/components/VideoRecorder/VideoRecorder.tsx
+++ src/components/VideoRecorder/VideoRecorder.tsx
@@ -4,11 +4,12 @@ const VideoRecorder = ({ videoSrc }: any) => {
       <video
         src={videoSrc}
         controls={false}
-        className="rounded-lg border-[#293060] rounded-tr-sm rounded-br-sm max-w-lg mx-auto"
+        className="rounded-lg border-[#293060] rounded-tr-sm rounded-br-sm height-large width-large max-w-lg mx-auto"
         style={{
           width: "600px",
           height: "600px",
           objectFit: "cover",
+          objectPosition: "20% 100%",
         }}
         autoPlay={true}
         loop={true}
